791106cedc39436be6fa223e21031fcec019f1a2
unknown <comp.student.it.2021@gmail.com>
2022-08-11 14:54:20 +0300

    feat: function guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return Math.ceil((this.min + this.max)/2)
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
